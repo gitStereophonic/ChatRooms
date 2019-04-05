a931379e5c3c7ae491bd7cf5e58e42c9a4458fb7
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-05 18:42:17 +0300

    [DONE] Filled Message List component with some dummy logic

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Message from './Message';
 
 const DUMMY_DATA = [
   {
@@ -19,7 +20,14 @@ class MessageList extends Component {
   render() {
     return (
       <div className="message-list">
-
+        {DUMMY_DATA.map((message, index) => {
+          return (
+            <div key={index} className="message">
+              <div className="message-username">{message.senderId}</div>
+              <div className="message-text">{message.text}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
