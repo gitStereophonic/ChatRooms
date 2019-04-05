c621e5b9781a2c21b4e9e7e61d0f18ab46d45890
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-05 20:46:46 +0300

    [ADD] Chatkit. (API differs)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Chatkit from '@pusher/chatkit-client';
 import MessageList from './components/MessageList';
 import SendMessageForm from './components/SendMessageForm';
 import RoomList from './components/RoomList';
 import NewRoomForm from './components/NewRoomForm';
 
+import { tokenUrl, instanceLocator } from './config';
+
 class App extends React.Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'raccoon_developer',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    });
+
+    chatManager.connect()
+      .then(currentUser => {
+        console.log(currentUser.rooms);
+        currentUser.subscribeToRoomMultipart({
+          roomId: '20147461',
+          messageLimit: 30,
+          hooks: {
+            onMessage: message => {
+              console.log("message text: ", message.parts[0].payload.content);
+            }
+          }
+        });
+      });
+  }
+
   render() {
     return (
       <div className="app">
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b27858b5-b1b7-44b7-bc48-4b074d2bf691/token";
+
+const instanceLocator = "v1:us1:b27858b5-b1b7-44b7-bc48-4b074d2bf691";
+
+export { tokenUrl, instanceLocator };
