53c6f7822af1f13b9deb8d7ff469172735070eb9
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-06 20:52:25 +0300

    [ADD] SendMessageForm controlled component

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
 class SendMessageForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messageText: ""
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    const { value } = event.target;
+    this.setState({
+      messageText: value
+    });
+  }
+
+  onSubmitHandler(event) {
+    event.preventDefault();
+    // send off the message here
+    console.log(this.state.messageText);
+  }
+
   render() {
     return (
-      <form className="send-message-form">
+      <form onSubmit={this.onSubmitHandler} className="send-message-form">
         <input
-          placeholder="SendMessageForm"
-          type="text" />
+          placeholder="Type your message"
+          type="text"
+          value={this.state.messageText}
+          onChange={this.onChangeHandler}
+        />
       </form>
     );
   }
