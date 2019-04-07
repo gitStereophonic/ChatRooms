95478c18e17dbe617989f7ea761008da819c836b
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-07 15:41:59 +0300

    [FIX] Rooms doesn't jump in list at switching

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 class RoomList extends React.Component {
   render() {
+    const orderedRooms = [
+      ...this.props.rooms.sort((left, right) => left.id > right.id)
+    ];
+
     return (
       <div className="rooms-list">
         <ul>
           <h3>Your rooms:</h3>
-          {this.props.rooms.map(room => {
+          {orderedRooms.map(room => {
             return (
               <li key={room.id} className="room">
                 <p
