1528c66557bc5082315ebde76a988d9cdfc292df
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-15 16:07:10 +0300

    [ADD] Empty room title component

---
+++ src/components/RoomTitle.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class RoomTitle extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="room-title">
+        
+      </div>
+    );
+  }
+}
+
+export default RoomTitle;
