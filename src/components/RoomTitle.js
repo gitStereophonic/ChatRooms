6d8667c9ae2ee2c3a56c8cab2b6384f227c0c9ad
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-05-01 01:16:45 +0300

    [UPD] RoomTitle is now function component

--- src/components/RoomTitle.js
+++ src/components/RoomTitle.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class RoomTitle extends Component {
-  constructor() {
-    super();
-    this.state = {
-
-    };
-  }
-
-  render() {
+function RoomTitle(props) {
+  if (!props.roomId)
     return (
-      <div className="room-title">
-        
-      </div>
+      <div className="room-title"></div>
     );
-  }
+
+  const members =
+    props.memCount === 1 ? " member" : " members";
+
+  console.log('RoomTitle members: ', members);
+
+  return (
+    <div className="room-title" onClick={props.onClickHandler}>
+      <h4>
+        {props.roomName}
+      </h4>
+      <p>
+        {props.memCount} {members}
+      </p>
+    </div>
+  );
 }
 
 export default RoomTitle;
