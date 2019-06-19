4063cdfe8eec36659f1f5748bd56ab19aaacd440
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-06-19 15:05:49 +0300

    [FIX] Right side widget is functional component now

--- src/components/RightSideWidget.js
+++ src/components/RightSideWidget.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class RightSideWidget extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: this.props.title
-    }
-  }
-
-  render() {
-    return (
-      <div className='right-side-widget'>
-        <h3>{this.state.title}</h3>
+function RightSideWidget(props) {
+  return (
+    <div className=
+      {
+        'right-side-widget ' +
+        (
+          props.isActive
+            ? 'right-side-widget-active'
+            : 'right-side-widget-non-active'
+        )
+      }
+    >
+      < h3 > {props.title}</h3 >
+      <div className='widget-body'>
+        <input type='checkbox'></input>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default RightSideWidget;
